63177f9759e2e8dc4336dc8d3efbf8272ba34853
Tom Genoni <tom@atomeye.com>
2020-04-24 23:07:50 -0700

    Update purgecss.config.js

--- config/purgecss.config.js
+++ config/purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     content: ['dist/index.html', 'dist/**/*.html'],
     css: ['dist/asset/css/root.css'],
-    whitelist: ['lazyloading', 'lazyloaded'],
+    whitelist: ['lazyloading', 'lazyloaded', 'is-active'],
 };
