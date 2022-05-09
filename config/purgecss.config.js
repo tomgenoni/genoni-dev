9f7a7cec3231dfe355a9c61e3eeaca3636567a3b
Tom Genoni <tom@genoni.dev>
2022-05-08 17:56:16 -0700

    Prettier on all files

--- config/purgecss.config.js
+++ config/purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['dist/index.html', 'dist/**/*.html'],
-    css: ['dist/asset/css/root.css'],
-    whitelist: ['lazyloading', 'lazyloaded', 'is-active'],
+  content: ['dist/index.html', 'dist/**/*.html'],
+  css: ['dist/asset/css/root.css'],
+  whitelist: ['lazyloading', 'lazyloaded', 'is-active'],
 };
